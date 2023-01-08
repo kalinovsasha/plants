3e2ac9d1fd8cd06af60bab3b5a535f9ec6d9a722
Kalinov Alexander <kalinovsasha@yandex.ru>
2023-01-08 21:39:25 +0300

    fix: bug with script

--- script/blurHandler.js
+++ script/blurHandler.js
@@ -1,6 +1,7 @@
 class BlurHandler {
   btns = [{}];
   imgs = [{}];
+  enabled = new Set();
 
   constructor(btns = [], imgs = []) {
     this.btns = btns;
@@ -9,11 +10,18 @@ class BlurHandler {
     this.btns.forEach((btn) =>
       btn.obj.addEventListener("click", (i) => {
         btn.obj.classList.toggle("service__content__btns_active");
-        imgs.forEach((img) => {
-          if (img.type == btn.type) {
-            img.img.classList.toggle("blur");
-          }
-        });
+
+        if (this.enabled.has(btn.type)) {
+          this.enabled.delete(btn.type);
+        } else this.enabled.add(btn.type);
+
+        if (this.enabled.size > 0) {
+          imgs.forEach((img) => {
+            if (this.enabled.has(img.type)) {
+              img.img.classList.remove("blur");
+            } else img.img.classList.add("blur");
+          });
+        } else imgs.forEach((img) => img.img.classList.remove("blur"));
       })
     );
   }
